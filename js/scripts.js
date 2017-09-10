1ad959a7608d9d720647f306cf16199c05d67f86
Ewelina Drozda <e.drozda@gmail.com>
2017-09-10 22:25:36 +0200

    poprawki w js i html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-var a = 2,
-	h = 3,
+var a = prompt('Enter a'),
+	h = prompt('Enter h'),
 	triangleArea = a*h/2;
+alert('Result of calculating is: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-var result = prompt('Do you agree?', + triangleArea);
+
